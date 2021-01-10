f1236b5d2381b565f8d9d01152381747aa63da83
Rachel Amos <rachelamos35@gmail.com>
2021-01-09 23:19:11 -0600

    adding function to call the types of characters to be included, as well as an alert if none are chosen.

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ var lowerChar = "abcdefghijklmnopqrstuvwxyz";
 var specialChar = "~!@#$%^&*(){}[]|<>?,./+=-_";
 var numberChar = "1234567890";
 var includedChar = "";
+var min = 8;
+var max = 128;
 
 // Write password to the #password input
 function writePassword() {
@@ -52,16 +54,28 @@ generateBtn.addEventListener("click", () => {
 // Alert if length is not between 8-128 characters long.
 function promptLength() {
     var pwLength = prompt("Please choose the length of your password.");
-    if (parseInt(pwLength) < 8 || parseInt(pwLength)
-    > 128) {
-        alert("Password length must be between " + 8 + " and " 128);
+    if (parseInt(pwLength) < min || parseInt(pwLength)
+    > max) {
+        alert("Password length must be between " + min + " and " + max);
         promptLength();
     } else length = pwLength
 }
 
 // Function to call the types of characters to be included.
 // Alert if none of the types are chosen.
+function promptChar() {
+    upper = confirm("Include uppercase letters?");
+    lower = confirm("Include lowercase letters?");
+    special = confirm("Include special characters?");
+    number = confirm("Include numbers?");
+
+    if (!upper && !lower && !special && !number) {
+        alert("You must include at least one type of character.");
+        promptChar();
+    }
+}
 
 // Function to get random compilation of all variables chosen.
+
 
 // Function to take random compilation and make it the length chosen by user.

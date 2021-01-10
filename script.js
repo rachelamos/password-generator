e9cb209396a383eaf381344a7be827f5aa35e3be
Rachel Amos <rachelamos35@gmail.com>
2021-01-09 19:15:51 -0600

    adding pseudocode and beginnings of button logic.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
+// Variables for each decision on what characters and how long to make the password... as well as what can be included within each "type" choice.
 var generateBtn = document.querySelector("#generate");
+
 var upper;
 var lower;
 var number;
@@ -22,4 +24,36 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Prompt user for length of password.
+// Prompt user for types of characters to be chosen.
+// Add types of selected characters together.
+generateBtn.addEventListener("click", () => {
+    promptLength();
+    promptChar();
+    includedChar = "";
+
+    if (upper) {
+
+    }
+    if (lower) {
+
+    }
+    if (special) {
+
+    }
+    if (number) {
+
+    }
+    console.log();
+    writePassword()
+});
+
+// Function to call the prompt for length.
+// Alert if length is not between 8-128 characters long.
+
+// Function to call the types of characters to be included.
+// Alert if none of the types are chosen.
+
+// Function to get random compilation of all variables chosen.
+
+// Function to take random compilation and make it the length chosen by user.
